57d25a8862f416d9e3c86e2df804511d0c62d1a4
xperia987 <t.i.tem987@gmail.com>
2019-08-16 12:03:22 +0400

    ff

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function (e) {
+
+    let buttons = $('.filterOption button');
+
+    $(buttons).on('click', function (e) {
+        let miss = 0;
+        $(this).toggleClass('active');
+        let filterCount = $(buttons).length;
+        $(buttons).each(function (key, val) {
+            if (!$(val).hasClass('active')) {
+                miss++;
+                $('.ssl[data-option="' + $(this).attr('data-filter') + '"]').hide();
+            } else {
+                $('.ssl[data-option="' + $(this).attr('data-filter') + '"]').show();
+            }
+        });
+        if (miss === filterCount) {
+            $('.ssl').show();
+        }
+    });
+
+    /*    $(document).on('scroll', function () {
+            console.log($(this).scrollTop());
+            if ($(this).scrollTop() >= 212) {
+                $('nav').addClass('sticky');
+            } else {
+                $('nav').removeClass('sticky');
+            }
+        }).trigger();*/
+
+    $('.profile > a').on('click', function (e) {
+        if ($(window).width() < 991) {
+            e.preventDefault();
+            $('ul.submenu').toggleClass('visible')
+        }
+    });
+
+
+
+});//ready
